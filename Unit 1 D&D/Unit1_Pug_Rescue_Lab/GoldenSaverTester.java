235c63007cb7b882ab17db3ccc02719df589189a
camilleaubrey <cacondren@outlook.com>
2025-08-27 13:05:50 -0700

    created tester and finished method

---
+++ Unit 1 D&D/Unit1_Pug_Rescue_Lab/GoldenSaverTester.java
@@ -0,0 +1,14 @@
+public class GoldenSaverTester {
+    
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Louis", "poodle");
+        Dog dog2 = new Dog("Fluffy", "goldendoodle");
+        Dog dog3 = new Dog("Scooter", "pug");
+        Dog dog4 = new Dog("Flash", "bulldog");
+        
+        // ArrayList<Dog> list = new ArrayList();
+        // {dog1, dog2, dog3, dog4};
+
+    }
+    
+}
--- Unit 1 D&D/Unit1_Pug_Rescue_Lab/PugSaver.java
+++ Unit 1 D&D/Unit1_Pug_Rescue_Lab/PugSaver.java
@@ -5,6 +5,19 @@ public class PugSaver {
 
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
+		if (list == null) {
+			System.out.println("There is no dogs in list. ");
+		}
 
+		ArrayList<Dog> goldenDogs = new ArrayList();
+		
+		for (Dog x : list) {
+			if (x.getBreed() == "Golden Retriever" || x.getBreed() == "Goldendoodle") {
+				list.remove(x);
+				goldenDogs.add(x);
+			}
+		}
+
+		list.addAll(goldenDogs);
 	}
 }
