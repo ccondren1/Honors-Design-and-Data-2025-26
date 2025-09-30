f2506c33dc1fd5c55ec450f76f3705c420e7316f
camilleaubrey <cacondren@outlook.com>
2025-09-30 10:49:13 -0700

    Create SinglyLinkedListTester.java

---
+++ Unit 2 D&D/SinglyLinkedList/SinglyLinkedListTester.java
@@ -0,0 +1,8 @@
+public class SinglyLinkedListTester {
+    
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList<>();
+        list.add(null);
+        System.out.println(list.toString());
+    }
+}
